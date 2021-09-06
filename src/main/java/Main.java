7682c4384d75596e27871314b0494f4e4a286241
2643raspi_no_owner <2643raspi@team2643.com>
2021-09-06 22:06:31 +0100

    init gradle, create SerialComm class

---
+++ src/main/java/Main.java
@@ -0,0 +1,12 @@
+package src.main.java;
+
+public class Main {
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		SerialComm.printPorts();
+		SerialComm sCommObj = SerialComm.getInstance();
+		// byte[] test = {0x2f, 0x5c};
+		// System.out.println(SerialComm.toHexString(test));
+		sCommObj.writeData(DatatypeOut.RIGHT_FRONT_MOTOR, 12984719);
+	}	
+}
---
+++ src/main/java/SerialComm.java
@@ -0,0 +1,106 @@
+package src.main.java;
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.util.concurrent.locks.ReentrantLock;
+import java.lang.Exception;
+import javax.xml.bind.DatatypeConverter;
+import com.fazecast.jSerialComm.*;
+
+public class SerialComm {
+    private static SerialComm single_instance = null;
+    private static SerialPort SerialIOPort = SerialPort.getCommPort("ttyACM0");
+    private static int numDatatypes = 0x25+2;
+    private static byte PKT_LEN = 0x11;
+
+    // init 2d array for each item + 2 status datatypes
+    private static byte[][] in_data = new byte[numDatatypes][8];
+    private static ReentrantLock[] mutexes = new ReentrantLock[numDatatypes];
+    private static byte packetNum = 0x00; // allow to overflow.
+    // we'll see if I need writeLock, much more of a pain to implement. - Probably an Asian
+    // private static ReentrantLock writeLock = new ReentrantLock();
+
+    public static byte[] HEADER = {0x2f, 0x5c};
+    public static byte[] VERSION = {0x00};
+    public static byte[] TRAILER = {(byte)0xad};
+
+    // ... |2HDR|1VER|1PKT#|1PKT_LEN(FUTURE_USE)|1DT|8DATA|2CHKSUM|1TRL| ...
+
+    public static void main(String[] args) {
+    }
+
+    public static SerialComm getInstance()
+    {
+        if (single_instance == null) {
+            // init mutexes to prevent corrupted data read
+            for(int i = 0; i < (numDatatypes); i++) {
+                mutexes[i] = new ReentrantLock();
+            }
+            single_instance = new SerialComm();
+            SerialIOPort.setBaudRate(115200);
+        }
+        return single_instance;
+    }
+
+    public void writeData(DatatypeOut DTOut, long data) {
+        if (data >= Long.MAX_VALUE) {
+            System.err.println("Data too large.");
+            return;
+        }
+
+        byte[] dataArray = longToByteArray(data);
+
+        writeData(DTOut, dataArray);
+    }
+
+    public void writeData(DatatypeOut DTOut, byte[] dataArray) {
+        byte[] writeBuffer = new byte[17];
+        System.arraycopy(HEADER, 0, writeBuffer, 0, HEADER.length);
+        System.arraycopy(VERSION, 0, writeBuffer, HEADER.length, VERSION.length);
+        writeBuffer[3] = packetNum++; // ret then increment
+        writeBuffer[4] = PKT_LEN;
+        writeBuffer[6] = (byte)DTOut.getValue();
+        System.arraycopy(dataArray, 0, writeBuffer, 6, dataArray.length);
+
+        long sum = 0;
+        for (int i = 0; i < 14; i++) {
+            sum += Byte.toUnsignedInt(writeBuffer[i]);
+        }
+        System.arraycopy(longToByteArray(sum), 6, writeBuffer, 14, 2);
+
+        System.arraycopy(TRAILER, 0, writeBuffer, 16, TRAILER.length);
+
+        System.out.println(toHexString(writeBuffer));
+        SerialIOPort.writeBytes(writeBuffer, 17);
+    }
+
+    public byte[] getData(int ID) {
+        mutexes[ID].lock();
+        try {
+            return in_data[ID];
+        } finally {
+            mutexes[ID].unlock();
+        }
+    }
+
+    public static void printPorts() {
+        SerialPort[] ports = SerialPort.getCommPorts();
+        for (SerialPort port: ports) {
+            System.out.println(port);
+        }
+    }
+
+    public static String toHexString(byte[] array) {
+        return DatatypeConverter.printHexBinary(array);
+    }
+    
+    public static byte[] toByteArray(String s) {
+        return DatatypeConverter.parseHexBinary(s);
+    }
+
+    public static byte[] longToByteArray(long l) {
+        ByteBuffer b = ByteBuffer.allocate(8);
+        b.order(ByteOrder.BIG_ENDIAN);
+        b.putLong(l);
+        return b.array();
+    }
+}
